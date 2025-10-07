ad39efeb3e7393158f29daf399a31a54ec96ac0e
Faldi <faldiansyahk@gmail.com>
2025-10-07 23:50:19 +0700

    first commit notes app

---
+++ lib/pages/notes_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NotesPage extends StatefulWidget {
+  const NotesPage({super.key});
+  
+  @override
+  State<NotesPage> createState() => _NotesPage();
+}
+
+class _NotesPage extends State<NotesPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

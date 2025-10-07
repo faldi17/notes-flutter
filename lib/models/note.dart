907479c2ef9129bf19eeb3b6d614f6c8c05a209e
Faldi <faldiansyahk@gmail.com>
2025-10-08 01:10:24 +0700

    note generate file

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,5 +1,11 @@
 import 'package:isar/isar.dart';
 
+// this line is needed to generate file
+// the run: dart run build_runner build
+part 'note.g.dart';
+
+@Collection()
 class Note {
   Id id = Isar.autoIncrement;
+  late String text;
 }

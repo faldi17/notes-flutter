ff2b55c4a5a58c2ebdd882d306e59f72201d0ccc
Faldi <faldiansyahk@gmail.com>
2025-10-08 22:23:00 +0700

    show dialog notes

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,16 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:notes_flutter/models/note_database.dart';
+import 'package:provider/provider.dart';
 
 class NotesPage extends StatefulWidget {
   const NotesPage({super.key});
-  
+
   @override
   State<NotesPage> createState() => _NotesPage();
 }
 
 class _NotesPage extends State<NotesPage> {
+  // text controller to access what the user typed
+  final textController = TextEditingController();
+
+  // create a note
+  void createNote() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(controller: textController),
+        actions: [
+          // create button
+          MaterialButton(
+            onPressed: () {
+              context.read<NoteDatabase>().addNote(textController.text);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  // read notes
+
+  // update a note
+
+  // delete a note
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('Notes')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNote,
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }

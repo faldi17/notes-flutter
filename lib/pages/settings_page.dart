ae14d82b55abe8e7d9d9fc96aff9e2b289d07f1a
Faldi <faldiansyahk@gmail.com>
2025-10-13 13:48:52 +0700

    switch toggle theme light/dark

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,10 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_flutter/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Row(
+        children: [
+          // dark mode
+          Text("Dark Mode"),
+
+          // switch toggle
+          CupertinoSwitch(
+            value: Provider.of<ThemeProvider>(context, listen: false).isDarkMode,
+            onChanged: (value) => Provider.of<ThemeProvider>(
+              context, listen: false
+            ).toggleTheme(),
+          ),
+        ],
+      ),
+    );
   }
 }
